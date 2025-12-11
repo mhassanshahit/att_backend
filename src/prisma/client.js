daab6156317f14cbe83de6af609159fbc532fd12
sultanranaitt <sultanranaitt@gmail.com>
2025-12-11 14:45:47 +0500

    updated prisma client

--- src/prisma/client.js
+++ src/prisma/client.js
@@ -1,8 +1,14 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['warn', 'error'],
-  errorFormat: 'pretty'
-});
+let prisma;
+
+if (!global.prisma) {
+  global.prisma = new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['warn', 'error'],
+    errorFormat: 'pretty'
+  });
+}
+
+prisma = global.prisma;
 
 module.exports = prisma;
