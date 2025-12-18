343d01d0df4176932faa61a64b311906303f0c2e
sultanranaitt <sultanranaitt@gmail.com>
2025-12-18 15:46:34 +0500

    request timer decreased

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ app.use(cors({
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 1000, // Limit each IP to 100 requests per windowMs
   message: {
     error: 'Too many requests from this IP, please try again later.'
   }
